aab1e9e75a6f1a3adbf835722bdf292c2208000e
legend524 <thecrazydev524@gmail.com>
2022-10-22 04:45:14 +0900

    updated progressbar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,14 +1,19 @@
-import React from 'react'
-
+import React from "react";
 
 class ProgressBar extends React.PureComponent {
   render() {
     return (
-      <div className='bg-gray-300 w-full h-6 dark:bg-gray-800 overflow-hidden' style={{borderRadius: '50px'}}>
-        <div className='h-full bg-alert-blue transition-all' style={{borderRadius: '50px', width: this.props.progress + '%'}} />
+      <div
+        className="bg-gray-300 w-full h-2 dark:bg-gray-800 overflow-hidden"
+        style={{ borderRadius: "30px" }}
+      >
+        <div
+          className="h-full bg-alert-blue transition-all"
+          style={{ borderRadius: "30px", width: this.props.progress + "%" }}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default ProgressBar
+export default ProgressBar;
