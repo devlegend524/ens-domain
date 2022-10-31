f04adb67cba21aebbc606aa7699e681e1f435c9c
ethancrypto <jonathan.cryptoguru@gmail.com>
2022-10-31 22:29:22 +0900

    fixed cart issue

--- src/services/cart/reducer.js
+++ src/services/cart/reducer.js
@@ -1,6 +1,6 @@
-import constants from './constants'
+import constants from "./constants";
 
-export const reducerName = 'cartService'
+export const reducerName = "cartService";
 
 export const initialState = {
   names: [],
@@ -8,32 +8,28 @@ export const initialState = {
   nameData: {},
   isRefreshingNameData: false,
   bulkRegistrationProgress: 0,
-  refreshNameDataProgress: 0,
-}
+  refreshNameDataProgress: 0
+};
 
 export const reducer = (state = initialState, action) => {
-  let index
+  let index;
   switch (action.type) {
     case constants.ADD_TO_CART:
-      if (state.names.indexOf(action.name) > -1) return state
+      if (state.names.indexOf(action.name) > -1) return state;
       return {
         ...state,
-        names: [
-          ...state.names,
-          action.name
-        ],
+        names: [action.name],
         quantities: {
-          ...state.quantities,
           [action.name]: 1
-        },
-      }
+        }
+      };
 
     case constants.REMOVE_FROM_CART:
-      index = state.names.indexOf(action.name)
+      index = state.names.indexOf(action.name);
       return {
         ...state,
-        names: [...state.names.slice(0, index), ...state.names.slice(index + 1)],
-      }
+        names: [...state.names.slice(0, index), ...state.names.slice(index + 1)]
+      };
 
     case constants.SET_QUANTITY:
       return {
@@ -42,7 +38,7 @@ export const reducer = (state = initialState, action) => {
           ...state.quantities,
           [action.name]: action.quantity
         }
-      }
+      };
 
     case constants.SET_NAME_DATA:
       return {
@@ -51,42 +47,42 @@ export const reducer = (state = initialState, action) => {
           ...state.nameData,
           [action.name]: action.data
         }
-      }
+      };
 
     case constants.ADD_BULK_REGISTRATIONS:
       return {
         ...state,
         names: [
           ...state.names,
-          ...action.names.filter(name => state.names.indexOf(name === -1)),
+          ...action.names.filter(name => state.names.indexOf(name === -1))
         ],
         nameData: {
           ...state.nameData,
-          ...action.nameData,
+          ...action.nameData
         },
         quantities: {
           ...state.quantities,
-          ...action.quantities,
-        },
-      }
+          ...action.quantities
+        }
+      };
 
     case constants.SET_BULK_REGISTRATION_PROGRESS:
       return {
         ...state,
         bulkRegistrationProgress: action.progress
-      }
+      };
 
     case constants.IS_REFRESHING_NAME_DATA:
       return {
         ...state,
         isRefreshingNameData: action.value
-      }
+      };
 
     case constants.SET_REFRESH_NAME_DATA_PROGRESS:
       return {
         ...state,
         refreshNameDataProgress: action.progress
-      }
+      };
 
     case constants.SET_NAME_DATA_BULK:
       return {
@@ -95,17 +91,17 @@ export const reducer = (state = initialState, action) => {
           ...state.nameData,
           ...action.nameData
         }
-      }
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
 
 const exports = {
-  reducer, 
+  reducer,
   reducerName,
-  initialState,
-}
+  initialState
+};
 
-export default exports
+export default exports;
